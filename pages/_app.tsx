04718ed6b95cef79faa4839be86c9f076d9e8dd6
ShinyJ375 <theglowstore@protonmail.com>
2022-07-18 08:06:16 +0200

    css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,15 +14,3 @@ function MyApp({ Component, pageProps }: AppProps) {
 }
 
 export default MyApp;
-
-<head>
-<div class="container">
-    <nav class="navbar">
-        <ul>
-            <li><a href="#home">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#service">Service</a></li>
-            <li><a href="#contact">Contact</a></li>
-          </ul>
-  </div>
-  </head>
